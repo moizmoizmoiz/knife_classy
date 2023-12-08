2d4ccc9d1e6a62694b370d06bf762f794ee1d14c
Moiz <57187188+moizmoizmoiz@users.noreply.github.com>
2023-12-08 05:40:27 +0000

    random erase augmentation

--- data.py
+++ data.py
@@ -7,7 +7,7 @@ from torchvision import transforms as T
 import torchvision.transforms.functional as F
 import pathlib
 from torchvision.io import read_image
-import numpy as np 
+import numpy as np
 import cv2
 
 # create dataset class
@@ -20,26 +20,33 @@ class knifeDataset(Dataset):
     def __len__(self):
         return len(self.images_df)
 
-    def __getitem__(self,index):
-        X,fname = self.read_images(index)
-        if not self.mode == "test":
+    def __getitem__(self, index):
+        X, fname = self.read_images(index)
+        if self.mode != "test":
             labels = self.images_df.iloc[index].Label
         else:
-            y = str(self.images_df.iloc[index].Id.absolute())
+            labels = None  # It seems you meant to use 'labels' instead of 'y'
+
         if self.mode == "train":
-            X = T.Compose([T.ToPILImage(),
-                    T.Resize((config.img_weight,config.img_height)),
-                    T.ColorJitter(brightness=0.2,contrast=0,saturation=0,hue=0),
-                    T.RandomRotation(degrees=(0, 180)),
-                    T.RandomVerticalFlip(p=0.5),
-                    T.RandomHorizontalFlip(p=0.5),
-                    T.ToTensor(),
-                    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])(X)
+            X = T.Compose([
+                T.ToPILImage(),
+                T.Resize((config.img_weight, config.img_height)),
+                T.ColorJitter(brightness=0.2, contrast=0, saturation=0, hue=0),
+                T.RandomRotation(degrees=(0, 180)),
+                T.RandomVerticalFlip(p=0.5),
+                T.RandomHorizontalFlip(p=0.5),
+                T.ToTensor(),
+                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+                T.RandomErasing(p=0.4, scale=(0.2, 0.2))
+            ])(X)
         elif self.mode == "val":
-            X = T.Compose([T.ToPILImage(),
-                    T.Resize((config.img_weight,config.img_height)),
-                    T.ToTensor(),
-                    T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])(X)
+            X = T.Compose([
+                T.ToPILImage(),
+                T.Resize((config.img_weight, config.img_height)),
+                T.ToTensor(),
+                T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ])(X)
+
         return X.float(), labels, fname
 
     def read_images(self,index):
